af51469c86fd6d4464f460016e32b7d1853601b2
Joao Soares <62054293+JoaoSoaresDev@users.noreply.github.com>
2021-07-27 15:25:49 +0800

    Add files via upload

---
+++ Client.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientDB
+{
+    class Client
+    {
+        private string lastName;
+        private string firstName;
+        private string secondName;
+        private int myAge;
+
+        private void SetNames( string name )
+        {
+            if( name != " ")
+            {
+                string[] names = name.Split(new string[] { " ", "." },
+                StringSplitOptions.RemoveEmptyEntries);
+                int n = names.Length;
+                // Length is the number of array elements (names)
+
+                if (n == 1) { firstName = names[0]; }
+                else if (n == 2) {
+                    firstName = names[0];
+                    lastName = names[1];
+                }
+                    else {
+                        firstName = names[0];
+                        secondName = names[1];
+                        lastName = names[2];
+                    }
+            }
+            
+        }
+
+        public string FullName
+        {
+            get { return firstName + " " + secondName + " " + lastName;  }
+            set { SetNames(value);  }
+        }
+
+        public int Age()
+        {
+            get { }
+        }
+
+        public Client( string name, int age )
+        {
+
+        }
+
+        public bool IsSame( string name )
+        {
+            string[] names = name.Split(new string[] { " ", "." },
+                StringSplitOptions.RemoveEmptyEntries);
+            int n = names.Length;
+            return lastName.Equals(names[n - 1]) && 
+                firstName.Equals(names[0]);
+        }
+
+    }
+}
---
+++ ClientDB.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientDB
+{
+    class Form1
+    {
+        private string companyName;
+        private int totalClients;       // Total number of clients maintained by this object
+        private const int MAX_CLIENTS = 100;
+        private Client[] myClients;     // An array containing the clients' object reference
+
+        public Form1( string companyName )
+        {
+            this.companyName = companyName;
+            totalClients = 0;
+            myClients = new Client[MAX_CLIENTS];
+        }
+
+        public bool AddClient( string clientName )
+        {
+            // Find any empty slots in myClient array
+            int index = -1;
+            for (int i = 0; i < MAX_CLIENTS; i++)
+                if (myClients[i] == null)
+                {
+                    index = i;
+                    break;
+                }
+            if(index != -1)
+            {
+                myClients[index] = new Client(clientName);
+                // Create a new client object
+
+                totalClients++;
+                return true;
+            }
+            return false;
+        }
+        
+        public bool RemoveClient(string clientName)
+        {
+            for(int i = 0; i < MAX_CLIENTS; i++)
+            {
+                if(myClients[i] != null)
+                {
+                    if(myClients[i].IsSame(clientName))
+                    {
+                        myClients[i] = null;
+                        // This client object is to be garbage collected
+                        totalClients--;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool IsAClient(string clientName)
+        {
+            for (int i = 0; i < MAX_CLIENTS; i++)
+                if (myClients[i] != null)
+                    if (myClients[i].IsSame(clientName))
+                        return true;
+            return false;
+        }
+
+
+
+    }
+}

c1aa20261944eb3a314c85bfd5f125a8757d792c
Joao Soares <62054293+JoaoSoaresDev@users.noreply.github.com>
2021-07-27 17:33:15 +0800

    Updated version.

---
+++ ClientDB/ClientDB.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ClientDB
+{
+    class ClientDB
+    {
+        private string companyName;
+        public int totalClients;       // Total number of clients maintained by this object
+        private const int MAX_CLIENTS = 100;
+        private Client[] myClients;     // An array containing the clients' object reference
+
+        public ClientDB(string companyName)
+        {
+            this.companyName = companyName;
+            totalClients = 0;
+            myClients = new Client[MAX_CLIENTS];
+        }
+
+        public bool AddClient(string clientName)
+        {
+            
+            // Find any empty slots in myClient array
+            int index = -1;
+            for (int i = 0; i < MAX_CLIENTS; i++)
+            {
+                if (myClients[i] == null)
+                {
+                    index = i;
+                    break;
+                }
+            }
+            if (index != -1)
+            {
+                myClients[index] = new Client(clientName);
+                // Create a new client object
+
+                totalClients++;
+                return true;
+            }
+            return false;
+        }
+
+        public bool RemoveClient(string clientName)
+        {
+            for (int i = 0; i < MAX_CLIENTS; i++)
+            {
+                if (myClients[i] != null)
+                {
+                    if (myClients[i].IsSame(clientName))
+                    {
+                        myClients[i] = null;
+                        // This client object is to be garbage collected
+                        totalClients--;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool IsAClient(string clientName)
+        {
+            for (int i = 0; i < MAX_CLIENTS; i++)
+                if (myClients[i] != null)
+                    if (myClients[i].IsSame(clientName))
+                        return true;
+            return false;
+        }
+
+        public bool ListAllClients(IList list)
+        {
+            bool check = false;
+
+            for (int i = 0; i < totalClients; i++)
+            {
+                list.Add(myClients[i].FullName);
+                check = true;
+            }
+
+            return check;
+
+        }
+    }
+}
---
+++ ClientDB/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClientDB;
+
+namespace ClientDB
+{
+    public partial class Form1 : Form
+    {
+        private ClientDB CDB;
+
+        public Form1()
+        {
+            InitializeComponent();
+            CDB = new ClientDB("Do Nothing Pty Ltd");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (!CDB.AddClient(ClientNameTextBox.Text))
+            {
+                MessageBox.Show("Cannot add this client!");
+            }
+            else
+                MessageBox.Show("Client added!");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!CDB.RemoveClient(ClientNameTextBox.Text))
+                MessageBox.Show("Cannot remove this client!");
+            else
+                MessageBox.Show("Client removed!");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (CDB.IsAClient(ClientNameTextBox.Text))
+                MessageBox.Show("Our client!");
+            else
+                MessageBox.Show("Not our client!");
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (ClientsList.Items != null)
+                ClientsList.Items.Clear();
+
+            if (!CDB.ListAllClients(ClientsList.Items))
+                MessageBox.Show("No clients in the database!");
+            else
+                MessageBox.Show(string.Format("Number of clients {0}", CDB.totalClients));
+        }
+
+        private void userInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
